679389cde5324327ad17585e2d54185d2c1ed43a
scottr@innoventsolutions.com <scottr@innoventsolutions.com@04e9051d-403f-7068-a84e-768cdc2c5b29>
2010-02-17 17:44:36 +0000

    Make compatible with Java 1.5

---
+++ branches/2.3.2/birt.functions.lib/src/innovent/birt/aggregations/CountUnique.java
@@ -0,0 +1,118 @@
+/** 
+ * Copyright (c) 2009 Innovent Solutions, Inc.
+ * 
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * 	Scott Rosenbaum - Innovent Solutions
+ *    Steve Schafer - Innovent Solutions
+ * 				 
+ */
+package innovent.birt.aggregations;
+
+import innovent.birt.aggregations.factory.InnoventParameterDefn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.birt.core.data.DataType;
+import org.eclipse.birt.data.engine.api.aggregation.Accumulator;
+import org.eclipse.birt.data.engine.api.aggregation.IAggrFunction;
+import org.eclipse.birt.data.engine.api.aggregation.IParameterDefn;
+import org.eclipse.birt.data.engine.core.DataException;
+
+/**
+ * Implements a custom Optimistic Sum Aggregation Function Provides a distinct
+ * count based on a single column value.
+ * 
+ * BIRT now supports function core.  Retained as an example of HowTo
+ */
+public class CountUnique implements IAggrFunction {
+
+	private static final InnoventParameterDefn dataValue = new InnoventParameterDefn("DataValue", "Data Value", false,
+			true, InnoventParameterDefn.CALCULATABLE, "The value that will be counted uniquely");
+
+	public int getDataType() {
+		return DataType.INTEGER_TYPE;
+	}
+
+	public Object getDefaultValue() {
+		return 0;
+	}
+
+	public int getNumberOfPasses() {
+		return 1;
+	}
+
+	public boolean isDataOrderSensitive() {
+		return false;
+	}
+
+	public String getName() {
+		return "COUNTUNIQUE";
+	}
+
+	public int getType() {
+		return SUMMARY_AGGR;
+	}
+
+	public String getDescription() {
+		return "Count Unique Values";
+	}
+
+	public String getDisplayName() {
+		return "Count Unique"; //$NON-NLS-1$
+	}
+
+	public IParameterDefn[] getParameterDefn() {
+		return new IParameterDefn[] { dataValue };
+	}
+
+	public Accumulator newAccumulator() {
+		return new CountUniqueAccumulator();
+	}
+
+	/**
+	 * Accumulate class does actual work of figuring out the number 
+	 * of unique values.
+	 * 
+	 */
+	private class CountUniqueAccumulator extends Accumulator {
+
+		private boolean isFinished = false;
+		private Set<Object> values = new HashSet<Object>();
+
+		public CountUniqueAccumulator() {
+			super();
+		}
+
+		public void start() throws DataException {
+			super.start();
+			values = new HashSet<Object>();
+			isFinished = false;
+		}
+
+		public void finish() throws DataException {
+			isFinished = true;
+		}
+
+		public void onRow(Object[] args) throws DataException {
+			assert (args.length > 0);
+			if (args[0] == null)
+				return;
+			values.add(args[0]);
+		}
+
+		public Object getValue() throws DataException {
+			if (!isFinished) {
+				throw new RuntimeException("Error! Call summary total function before finished the dataset");
+			}
+			return new Integer(values.size());
+		}
+	}
+
+}
---
+++ branches/2.3.2/birt.functions.lib/src/innovent/birt/functions/factory/InnoventFunction.java
@@ -0,0 +1,146 @@
+/** 
+ * Copyright (c) 2009 Innovent Solutions, Inc.
+ * 
+ * All rights reserved. 
+ * 
+ * This program and the accompanying materials are made available under the terms 
+ * of the Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ * 	Scott Rosenbaum - Innovent Solutions
+ *  Steve Schafer - Innovent Solutions
+ * 				 
+ */
+package innovent.birt.functions.factory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+
+import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.core.script.functionservice.IScriptFunctionContext;
+import org.eclipse.birt.core.script.functionservice.IScriptFunctionExecutor;
+import org.eclipse.birt.report.engine.api.IEngineTask;
+import org.eclipse.birt.report.engine.api.script.IReportContext;
+import org.eclipse.birt.report.engine.executor.ExecutionContext;
+
+/**
+ * Common super-class used to create convenience methods for use in other
+ * functions.
+ * 
+ */
+public abstract class InnoventFunction implements IScriptFunctionExecutor {
+	private static final Logger logger = Logger.getLogger(InnoventFunction.class.getName());
+
+	public abstract Object execute(Object[] arguments, IScriptFunctionContext context) throws BirtException;
+
+	/**
+	 * When the DataSet editor runs, modifications to the queryText as done in the BindParameter function
+	 * will get written to the queryText.  To avoid this, we test if the query is being run by the DataSet
+	 * editor.
+	 * 
+	 * BIRT 2.5 has added a new Task type that identifies the DataSet editor so that this code is no longer 
+	 * required. 
+	 *  
+	 * @param reportContext
+	 * @return
+	 */
+	public static boolean isDataSetEditor(IReportContext reportContext) {
+		try {
+
+			// get the protect field 'context' from reportContext
+			@SuppressWarnings("unchecked")
+			Class rciClass = reportContext.getClass();
+			Field fieldFromScript = rciClass.getDeclaredField("context");
+			if (fieldFromScript == null) {
+				throw new NoSuchFieldException("Reporting Access to context from IReportContext");
+			}
+			// instantiate the ExecutionContext object that
+			// populates the context field
+			fieldFromScript.setAccessible(true);
+			Object execContext = fieldFromScript.get(reportContext);
+			ExecutionContext ectx = (ExecutionContext) execContext;
+
+			IEngineTask et = ectx.getEngineTask();
+			if ("DummyEngineTask".equalsIgnoreCase(et.getClass().getSimpleName())) {
+				logger.info("Is Data Set Editor");
+				return true;
+			}
+
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.finest("Is Not a Data Set Editor");
+
+		return false;
+	}
+
+	/**
+	 * Get a handle to the reportContext. This is a common error and trickier
+	 * then one may think, best to wrap off with a good implementation
+	 * 
+	 * @param rcArgument
+	 * @param sqlText
+	 * @return
+	 * @throws BirtException
+	 */
+	protected IReportContext getReportContext(final Object rcArgument) throws BirtException {
+		if (rcArgument == null) {
+			throw new BirtException(InnoventFunctionFactory.plugin_id, "ReportContext object is null in "
+					+ this.getClass().getSimpleName(), InnoventFunctionFactory.getResourceBundle());
+		}
+		if ((rcArgument instanceof IReportContext) != true) {
+			throw new BirtException(InnoventFunctionFactory.plugin_id,
+					"ReportContext is not an instance of IReportContext in " + this.getClass().getSimpleName(),
+					InnoventFunctionFactory.getResourceBundle());
+		}
+		logger.finest("Found report context");
+		return (IReportContext) rcArgument;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void addBirtException(IReportContext reportContext, String errorMessage, Integer severity) {
+		BirtException be = new BirtException("org.eclipse.birt.report.engine", errorMessage, new Object[] { "" });
+		be.setSeverity(severity);
+
+		try {
+			// get the protect field 'context' from reportContext
+			Class rciClass = reportContext.getClass();
+			Field fieldFromScript = rciClass.getDeclaredField("context");
+			if (fieldFromScript == null) {
+				return;
+			}
+
+			// instantiate the ExecutionContext object that
+			// populates the context field
+			fieldFromScript.setAccessible(true);
+			Object execContext = fieldFromScript.get(reportContext);
+
+			// now get a handle to the addException method on ExecutionObject
+			Class execClass = execContext.getClass();
+			Method addMethod = execClass.getMethod("addException", new Class[] { BirtException.class });
+
+			// finally invoke the method which will add the BirtException 
+			// to the report
+			addMethod.setAccessible(true);
+			addMethod.invoke(execContext, new Object[] { be });
+
+			// Lots of ways for this to break...
+		} catch (Exception e) {
+			logger.warning(e.getMessage());
+			e.printStackTrace();
+		}
+		return;
+	}
+}
